5a842919719b899ccab44b983e09022fc9a2721a
Leonardo José do Nascimento Guimarães <leojnguimaraes@gmail.com>
2021-10-22 17:13:10 -0300

    Meu Primeiro Commit

---
+++ hello_world/hello_world.f
@@ -0,0 +1,24 @@
+      PROGRAM Hello_World
+      USE OMP_LIB
+
+      IMPLICIT NONE
+
+      INTEGER :: i, thread_id
+
+      !$OMP PARALLEL PRIVATE(thread_id)
+
+      thread_id = OMP_GET_THREAD_NUM()
+
+      DO i=0,OMP_GET_MAX_THREADS()
+        IF (i == thread_id) THEN
+            PRINT *, "Hello from process: ", thread_id
+        END IF
+        !$OMP BARRIER
+      END DO
+
+      !$OMP END PARALLEL
+
+
+      END
+
+c     Fonte: https://curc.readthedocs.io/en/latest/programming/OpenMP-Fortran.html

c34d882b798705fcc4e21daaedacb673dc668df5
Leonardo José do Nascimento Guimarães <leojnguimaraes@gmail.com>
2021-11-05 15:35:06 -0300

    adding barrier:  hello_world for MPI and Coarray

--- hello_world_coarray/hello_world_coarray.f
+++ hello_world_coarray/hello_world_coarray.f
@@ -1,5 +1,12 @@
       program hello_world
+      
       implicit none
-        write(*,*) 'Hello world from ', 
-     .   this_image() , 'of', num_images()
+
+      integer :: i
+
+      do i=1,num_images()
+        if (i==this_image())
+     .  write(*,*) 'Hello world from ',this_image(),'of',num_images()
+      enddo
+      
       end program hello_world

4ad5c9f61e7896053af77a0be7d82efb559b88d4
Leonardo José do Nascimento Guimarães <leojnguimaraes@gmail.com>
2021-11-06 03:08:05 -0300

    update soma_acc.f

--- soma_acc/soma_acc.f
+++ soma_acc/soma_acc.f
@@ -6,13 +6,13 @@
 
       REAL*8 total_Sum 
 
-      INTEGER i, c1, c2, c3, cgpu, chost
+      INTEGER i, c1, c2, cgpu, chost
 
       !call acc_init( acc_device_nvidia )
       call system_clock( count=c1 )
       total_Sum = 0.0d0
       !$acc parallel loop reduction (+:total_Sum)
-      DO i=1,10000
+      DO i=1,10000000
         total_Sum = total_Sum + 1.0d0
       END DO
       !$acc end parallel loop 
@@ -22,11 +22,12 @@
       PRINT *, 'Total Sum:', total_Sum
 
       total_Sum = 0.0d0
-      DO i=1,10000
+      call system_clock( count=c1 )
+      DO i=1,10000000
         total_Sum = total_Sum + 1.0d0
       END DO
-      call system_clock( count=c3 )
-      chost = c3 - c2
+      call system_clock( count=c2 )
+      chost = c2 - c1
       PRINT *, chost, ' microseconds on host'
       PRINT *, 'Total Sum:', total_Sum
 

9e53ea022303a3e86bd9c67a8eca544d9ad7d2b5
Leonardo José do Nascimento Guimarães <leojnguimaraes@gmail.com>
2021-11-06 15:55:03 -0300

    adding 'sync all'  hello_world_coarray

--- hello_world_coarray/hello_world_coarray.f
+++ hello_world_coarray/hello_world_coarray.f
@@ -7,6 +7,7 @@
       do i=1,num_images()
         if (i==this_image())
      .  write(*,*) 'Hello world from ',this_image(),'of',num_images()
+        sync all
       enddo
       
       end program hello_world
